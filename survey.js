c8927dc4ed92e285b8bdde97daaa180d70320fcb
Zio <zi.ao.tan@outlook.com>
2021-05-06 09:38:53 -0700

    updated version (nally's version)

--- survey.js
+++ survey.js
@@ -5,23 +5,43 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+const answers = [];
+
 rl.question('What\'s your name? Nicknames are also acceptable:)', (answer) => {
   console.log(`Thank you for your answer: ${answer}`);
+  answers.push(answer);
   rl.question('What do you think of Node.js?: ', (answer) => {
     console.log(`Thank you for your valuable feedback: ${answer}`);
+    answers.push(answer);
     rl.question('What\'s an activity you like doing?: ', (answer) => {
       console.log(`Thank you for your valuable feedback: ${answer}`);
+      answers.push(answer);
       rl.question('What do you listen to while doing that?: ', (answer) => {
         console.log(`Thank you for your valuable feedback: ${answer}`);
+        answers.push(answer);
         rl.question('Which meal is your favourite? (eg: dinner, brunch, etc.): ', (answer) => {
           console.log(`Thank you for your valuable feedback: ${answer}`);
+          answers.push(answer);
           rl.question('What\'s your favourite thing to eat for that meal?: ', (answer) => {
             console.log(`Thank you for your valuable feedback: ${answer}`);
+            answers.push(answer);
             rl.question('Which sport is your absolute favourite?: ', (answer) => {
               console.log(`Thank you for your valuable feedback: ${answer}`);
+              answers.push(answer);
               rl.question('What is your superpower? In a few words, tell us what you are amazing at!: ', (answer) => {
                 console.log(`Thank you for your valuable feedback: ${answer}`);
+                answers.push(answer);
                 rl.close();
+                const node = answers[0];
+                const name = answers[1];
+                const activity = answers[2];
+                const music = answers[3];
+                const meal = answers[4];
+                const food = answers[5];
+                const sport = answers[6];
+                console.log();
+                console.log('Your profile is ready!!');
+                console.log(`My name is ${name} and I really like ${activity}! I listen to ${music} while eating ${food} during ${meal} and playing ${sport}... and I think Node is ${node}!!`);
                 });
               });
             });
